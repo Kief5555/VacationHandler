60748f09f49f67277b69a1e91c563d2ff578dff0
Kief <67562560+Kief5555@users.noreply.github.com>
2024-08-11 21:18:07 +0900

    Fix var const

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.get('/cities', authMiddleware, (req, res) => {
 
 // GET route to view all images
 app.post('/images', authMiddleware, (req, res) => {
-    const allImages = {};
+    let allImages = {};
     fs.readdirSync(IMAGES_DIR).forEach((city) => {
         //Add the images to the allImages object without the city name in the key so we just add to the object
         allImages = { ...allImages, ...getImages(city) };
